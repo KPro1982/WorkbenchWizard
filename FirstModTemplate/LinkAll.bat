e779ea3711d4c70dfcc672b0af40255a402f5fb0
kpro1982 <daniel_cravens@hotmail.com>
2022-12-13 11:39:20 -0800

    Refactored so that wizard does not need to run with admin priv
    Refactored so wizard does not need to run as admin in order to link files. Fixed  bug that caused linkall not to work in some situations.

--- FirstModTemplate/LinkAll.bat
+++ FirstModTemplate/LinkAll.bat
@@ -9,6 +9,6 @@ mklink /J "P:\FirstMod\" "WB_SOURCEFOLDER"
 mklink /J "WB_DAYZFOLDER\FirstMod\" "WB_SOURCEFOLDER" 
 
 echo linking packed pbos
-mklink /J "P:\@FirstMod\" "WB_DAYZFOLDER\@FirstMod\" 
+mklink /J "P:\@FirstMod\" "WB_PACKEDMOD" 
 
 
